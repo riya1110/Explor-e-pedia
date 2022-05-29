3daefc2fcb450387a4eecc3bbc57b85f4a882a97
riya1110 <76813789+riya1110@users.noreply.github.com>
2022-05-29 11:56:49 -0400

    Add files via upload

---
+++ Explor-e-pedia/index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!-- Meta tags -->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+	<!-- Title -->
+	<title>Explor-e-pedia</title>
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="images/logo/favicon.ico">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="css/font-awesome/css/fontawesome.min.css">
+
+	<!-- FontAwesome kit -->
+	<script src="https://kit.fontawesome.com/4fa77e6d3c.js" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Responsive CSS -->
+	<link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+
+	<section id="home-page">
+
+		<!-- Header -->
+		<header class="site-header">
+			<div class="container">
+				<div class="header-wrapper">
+
+					<!-- Mobile Menu Open Button -->
+					<span id="mobile-nav-open-btn">&#9776;</span>
+
+					<div class="logo-wrapper">
+
+						<a href="#" title="Site Logo">
+							<img src="images/logo/dark-logo.png" alt="Logo">
+						</a>
+					</div>
+					<div class="navigation-wrapper">
+						<nav class="main-nav">
+							<ul class="menu-navigation">
+								<li>
+									<a href="shared/index-search-places">Search Places</a>
+								</li>
+								<li>
+									<a href="shared/faq">FAQ</a>
+								</li>
+								<li>
+									<a href="shared/contact-us">Contact Us</a>
+								</li>
+								<li>
+									<a class="btn btn-general btn-purple" href="shared/login" title="Download" role="button">Login</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+
+					<!-- Mobile Menu -->
+					<div id="mobile-nav">
+
+						<img class="logo-in-mobile-menu" src="images/logo/dark-logo.png" alt="Notes Logo">
+
+						<!-- Mobile Menu Close Button -->
+						<span id="mobile-nav-close-btn">&times;</span>
+
+						<div id="mobile-nav-content">
+							<ul class="nav">
+								<li>
+									<a href="pages/search-places">Search Places</a>
+								</li>
+								<li>
+									<a href="pages/faq">FAQ</a>
+								</li>
+								<li>
+									<a href="pages/contact-us">Contact Us</a>
+								</li>
+								<li>
+									<a class="btn btn-general btn-purple" href="pages/login" title="Download" role="button">Login</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- Header ends -->
+
+		<!-- Home -->
+		<section id="home">
+		
+			<!-- Background Image -->
+			<img id="home-bg" src="images/Homepage/banner-with-overlay.jpg" alt="Background">
+
+			<!-- Home content -->
+			<div class="container">
+				<div id="home-content">
+
+					<div id="home-heading">
+						<h1>Discover the faces <br>of the world</h1>
+					</div>
+
+					<div id="home-text"> 
+						<p>Search Chat Navigate and Travel 	!!</p>
+					</div>
+
+					<a class="btn btn-general btn-home" href="pages/faq" title="Learn More" role="button">LEARN MORE</a>
+					
+				</div>
+			</div>
+
+		</section>
+
+		<!-- About -->
+		<section id="about">
+			<div class="content-box-lg">
+				<div class="container">
+					<div class="row">
+					
+						<!-- left side -->
+						<div class="col-md-4 col-sm-12 col-12">
+							<div id="about-left">
+								<p>About<br>Explor-e-pedia</p>
+							</div>
+						</div>
+
+						<!-- right-side -->
+						<div class="col-md-8 col-sm-12 col-12">
+							<div id="about-right">
+								<p>The main objective is to build a web applicaiton in which we can show different places and also add various famous places like restaurants, cafes, shopping mall, tourist places, etc. Moreover,we can search for any famous places of a particular area and can comment and read reviews. Also, we can get ratings for that place.</p>
+								<p>This web application will provide up-to-date information. It will display the latest results at all time and if it lags behind the user will be notified. User needs to sign-in to their account to prove their identity. The security features from anonymous login for all users to access the application’s full features. So the chances of the application getting intruded are very less.</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- About ends -->
+
+		<!-- Testimonial -->
+		<section id="testimonial">
+			<div class="content-box-lg">
+			
+				<p class="section-heading">What our Customers are Saying</p>
+				
+				<div class="container">
+					<div class="row no-gutters">
+					
+						<div class="col-md-6">
+							<div id="testimonial-tile-1" class="author">
+								<div class="row">
+								
+									<div class="col-md-3 col-sm-2 col-2 author-photo">
+										<img src="images/testimonial/customer-1.png" alt="client" class="rounded-circle">
+									</div>
+									
+									<div class="col-md-9 col-sm-10 col-10">
+										<div class="author-name-des">
+											<p>Walter Meller</p>
+											<p>Founder & CEO, Matrix Group</p>
+										</div>
+									</div>
+									
+								</div>
+								
+								<p id="testimonial-text">"Lorem ipsum dolor sit erys amet tryue consectetur adipisicing elit. Ratione, facere getif rice cumque officiis necessitatibus blanditiis. Culpa."</p>
+							</div>
+						</div>
+						
+						<div class="col-md-6">
+							<div id="testimonial-tile-2" class="author">
+								<div class="row">
+								
+									<div class="col-md-3 col-sm-2  col-2 author-photo">
+										<img src="images/testimonial/customer-2.png" alt="client" class="rounded-circle">
+									</div>
+									
+									<div class="col-md-9 col-sm-10  col-10">
+										<div class="author-name-des">
+											<p>Jonnie Riley</p>
+											<p>Employee, Curious Snakes</p>
+										</div>
+									</div>
+								</div>
+								
+								<p id="testimonial-text">"Lorem ipsum dolor sit erys amet tryue consectetur adipisicing elit. Ratione, facere getif rice cumque officiis necessitatibus blanditiis. Culpa."</p>
+							</div>
+						</div>
+						
+						<div class="col-md-6">
+							<div id="testimonial-tile-3" class="author">
+								<div class="row">
+								
+									<div class="col-md-3 col-sm-2  col-2 author-photo">
+										<img src="images/testimonial/customer-3.png" alt="client" class="rounded-circle">
+									</div>
+									
+									<div class="col-md-9 col-sm-10  col-10">
+										<div class="author-name-des">
+											<p>Amilia Luna</p>
+											<p>Teacher, Saint Joseph High School</p>
+										</div>
+									</div>
+								</div>
+								
+								<p id="testimonial-text">"Lorem ipsum dolor sit erys amet tryue consectetur adipisicing elit. Ratione, facere getif rice cumque officiis necessitatibus blanditiis. Culpa."</p>
+							</div>
+						</div>
+						
+						<div class="col-md-6">
+							<div id="testimonial-tile-4" class="author">
+								<div class="row">
+								
+									<div class="col-md-3 col-sm-2  col-2 author-photo">
+										<img src="images/testimonial/customer-4.png" alt="client" class="rounded-circle">
+									</div>
+									
+									<div class="col-md-9 col-sm-10  col-10">
+										<div class="author-name-des">
+											<p>Daniel Cardos</p>
+											<p>Softwere Engineer, Infitium Company</p>
+										</div>
+									</div>
+								</div>
+								
+								<p id="testimonial-text">"Lorem ipsum dolor sit erys amet tryue consectetur adipisicing elit. Ratione, facere getif rice cumque officiis necessitatibus blanditiis. Culpa."</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+				
+			</div>
+		</section>
+		<!-- Testimonial ends -->
+
+		<!-- Footer  -->
+		<footer>
+			<div id="home-footer">
+				<div class="container">
+					<div class="row">
+					
+						<div class="col-md-6 col-sm-8 col-12 text-left">
+							<p>Copyright &copy; Explor-e-pedia All Rights Reserved.</p>
+						</div>
+						
+						<div class="col-md-6 col-sm-4 col-12 text-right">
+							<ul class="social-list">
+							
+								<li>
+									<a class="facebook-img" href="#">
+										<img src="images/Homepage/facebook.png" alt="">
+									</a>
+								</li>
+								
+								<li>
+									<a class="twitter-img" href="#">
+										<img src="images/Homepage/twitter.png" alt="">
+									</a>
+								</li>
+								
+								<li>
+									<a class="linkedin-img" href="#">
+										<img src="images/Homepage/linkedin.png" alt="">
+									</a>
+								</li>
+							</ul>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- Footer Ends -->
+
+	</section>
+
+	<!-- Java-scripts -->
+
+	<!-- JQuery -->
+	<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap JS -->
+	<script src="js/bootstrap/bootstrap.min.js"></script>
+
+	<!-- Custom JS -->
+	<script src="js/script.js"></script>
+
+	<!-- Java-scripts -->
+</body>
+
+</html>
